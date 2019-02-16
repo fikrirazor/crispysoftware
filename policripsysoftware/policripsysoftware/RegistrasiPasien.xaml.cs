abc591b13ce8944e5f7f8b0e1adf059cff187c2a
fikrirazor <fikrirazor@outlook.co.id>
2019-02-16 22:53:38 +0700

    add inteface registrasi pasien

--- policripsysoftware/policripsysoftware/RegistrasiPasien.xaml.cs
+++ policripsysoftware/policripsysoftware/RegistrasiPasien.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace policripsysoftware
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window2.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window2 : Window
     {
-        public MainWindow()
+        public Window2()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click()
-        {
-
         }
     }
 }
