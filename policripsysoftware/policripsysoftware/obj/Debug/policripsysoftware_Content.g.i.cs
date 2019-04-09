68620a3b764a817e102f5fbe598f56740e4cc253
fikrirazor <fikrirazor@outlook.co.id>
2019-04-09 18:18:30 +0700

    update

--- policripsysoftware/policripsysoftware/obj/Debug/policripsysoftware_Content.g.i.cs
+++ policripsysoftware/policripsysoftware/obj/Debug/policripsysoftware_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("stethoscope.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("023-hospital.png")]
 
 
