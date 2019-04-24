cacc904c584e2fc759c8d2fd9ebea6956596a790
fikrirazor <fikrirazor@outlook.co.id>
2019-04-24 22:52:19 +0700

    edit pasien,pendaftaran lama baru, dan kelola pengguna

--- policripsysoftware/policripsysoftware/Pasien.cs
+++ policripsysoftware/policripsysoftware/Pasien.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows;
+using System.Data;
+using System.Windows.Controls;
 
 namespace policripsysoftware
 {
@@ -72,11 +74,64 @@ namespace policripsysoftware
                 throw new Exception(ex.Message);
             }
         }
-        public void delete()
+        public void add()
         {
-            
+            string dbPath = System.Environment.CurrentDirectory + "\\DB";
+            string dbFilePath = dbPath + "\\poliklinik.db";
+            SQLiteConnection sql_con = new SQLiteConnection(string.Format("Data Source={0};", dbFilePath));
+            sql_con.Open();
+            SQLiteCommand Query = new SQLiteCommand("insert into pasien(nama,tanggallahir,nohp,noktp) values(@b,@c,@d,@e)", sql_con);
+            Query.Parameters.AddWithValue("@b", nama);
+            Query.Parameters.AddWithValue("@c", tanggallahir);
+            Query.Parameters.AddWithValue("@d", nohp);
+            Query.Parameters.AddWithValue("@e", noktp);
+            try
+            {
+                Query.ExecuteNonQuery();
+                MessageBox.Show("Pendaftaran Berhasil!");
+                PendaftaranBaru pbw = new PendaftaranBaru();
+                pbw.Close();
+                MainMenu pw = new MainMenu();
+                pw.Show();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
-        
+        public void delete(DataGrid dataGrid)
+        {
+            string dbPath = System.Environment.CurrentDirectory + "\\DB";
+            string dbFilePath = dbPath + "\\poliklinik.db";
+            SQLiteConnection sql_con = new SQLiteConnection(string.Format("Data Source={0};", dbFilePath));
+            sql_con.Open();
+
+            if (dataGrid.SelectedCells.Count == 0)
+                return;
+
+            SQLiteCommand comm = new SQLiteCommand("delete from pasien where nopasien= @rownopasien", sql_con);
+            SQLiteDataAdapter da = new SQLiteDataAdapter(comm);
+
+        }
+        public void update(DataGrid dataGrid)
+        {
+
+        }
+        public void view(DataGrid dataGrid)
+        {
+            string dbPath = System.Environment.CurrentDirectory + "\\DB";
+            string dbFilePath = dbPath + "\\poliklinik.db";
+            SQLiteConnection sql_con = new SQLiteConnection(string.Format("Data Source={0};", dbFilePath));
+            sql_con.Open();
+            SQLiteCommand comm = new SQLiteCommand("Select * From pasien", sql_con);
+            SQLiteDataAdapter da = new SQLiteDataAdapter(comm);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            DataTable dt = ds.Tables[0];
+            dataGrid.ItemsSource = dt.AsDataView();
+        }
+
+
     }
 }
